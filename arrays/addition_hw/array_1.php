f7769b1dfdc2908dd16f2a4b842d7c8652d14c31
Anton <33983243+saddevil@users.noreply.github.com>
2017-12-25 23:08:37 +0300

    Update array_1.php

--- arrays/addition_hw/array_1.php
+++ arrays/addition_hw/array_1.php
@@ -1,50 +1,86 @@
 <?php
-require 'arrays.php';
-?>
 
-<!doctype html>
-<html>
-<head>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <title>Menu</title>
-</head>
-<body>
-<div class="container">
-<ul>
-    <li><a href="<?=$menu['0']['link_1']?>"><?=$menu['0']['name_1']?></a>
-        <ul>
-            <li><a href="<?=$menu['0']['inside_1_1']['link_4']?>"><?=$menu['0']['inside_1_1']['name_4']?></a>
-                <ul>
-                    <li><a href="<?=$menu['0']['inside_1_1']['inside_4_1']['link_9']?>"><?=$menu['0']['inside_1_1']['inside_4_1']['name_9']?></a></li>
-                    <li><a href="<?=$menu['0']['inside_1_1']['inside_4_2']['link_10']?>"><?=$menu['0']['inside_1_1']['inside_4_2']['name_10']?></a></li>
-                </ul>
-            </li>
-            <li><a href="<?=$menu['0']['inside_1_2']['link_5']?>"><?=$menu['0']['inside_1_2']['name_5']?></a></li>
-        </ul>
-    </li>
-    <li><a href="<?=$menu['1']['link_2']?>"><?=$menu['1']['name_2']?></a>
-        <ul>
-            <li><a href="<?=$menu['1']['inside_2_1']['link_6']?>"><?=$menu['1']['inside_2_1']['name_6']?></a></li>
-            <li><a href="<?=$menu['1']['inside_2_2']['link_7']?>"><?=$menu['1']['inside_2_2']['name_7']?></a>
-                <ul>
-                    <li><a href="<?=$menu['1']['inside_2_2']['inside_7_1']['link_11']?>"><?=$menu['1']['inside_2_2']['inside_7_1']['name_11']?></a></li>
-                    <li><a href="<?=$menu['1']['inside_2_2']['inside_7_2']['link_12']?>"><?=$menu['1']['inside_2_2']['inside_7_2']['name_12']?></a></li>
-                </ul>
-            </li>
-        </ul>
-    </li>
-    <li><a href="<?=$menu['2']['link_3']?>"><?=$menu['2']['name_3']?></a>
-        <ul>
-            <li><a href="<?=$menu['2']['inside_3_1']['link_8']?>"><?=$menu['2']['inside_3_1']['name_8']?></a>
-                <ul>
-                    <li><a href="<?=$menu['2']['inside_3_1']['inside_8_1']['link_13']?>"><?=$menu['2']['inside_3_1']['inside_8_1']['name_13']?></a></li>
-                    <li><a href="<?=$menu['2']['inside_3_1']['inside_8_2']['link_14']?>"><?=$menu['2']['inside_3_1']['inside_8_2']['name_14']?></a></li>
-                    <li><a href="<?=$menu['2']['inside_3_1']['inside_8_3']['link_15']?>"><?=$menu['2']['inside_3_1']['inside_8_3']['name_15']?></a></li>
-                </ul>
-            </li>
-        </ul>
-    </li>
-</ul>
-</div>
-</body>
-</html>
+//объявляем массив, содержащий структуру меню
+//для простоты понимания, объявляем частями
+$element_1 = array(
+    "link_1"=>"ref1.php",
+    "name_1"=>"Элемент 1",
+    "inside_1_1"=>array(
+        "link_4"=>"ref4.php",
+        "name_4"=>"Элемент 4",
+        "inside_4_1"=>array(
+            "link_9"=>"ref9.php",
+            "name_9"=>"Элемент 9"
+        ),
+        "inside_4_2"=>array(
+            "link_10"=>"ref10.php",
+            "name_10"=>"Элемент 10"
+        )
+    ),
+    "inside_1_2"=>array(
+        "link_5"=>"ref5.php",
+        "name_5"=>"Элемент 5"
+    )
+);
+$element_2 = array(
+    "link_2"=>"ref2.php",
+    "name_2"=>"Элемент 2",
+    "inside_2_1"=>array(
+        "link_6"=>"ref6.php",
+        "name_6"=>"Элемент 6"
+    ),
+    "inside_2_2"=>array(
+        "link_7"=>"ref7.php",
+        "name_7"=>"Элемент 7",
+        "inside_7_1"=>array(
+            "link_11"=>"ref11.php",
+            "name_11"=>"Элемент 11"
+        ),
+        "inside_7_2"=>array(
+            "link_12"=>"ref12.php",
+            "name_12"=>"Элемент 12"
+        )
+    )
+);
+$element_3 = array(
+    "link_3"=>"ref3.php",
+    "name_3"=>"Элемент 3",
+    "inside_3_1"=>array(
+        "link_8"=>"ref8.php",
+        "name_8"=>"Элемент 8",
+        "inside_8_1"=>array(
+            "link_13"=>"ref13.php",
+            "name_13"=>"Элемент 13"
+        ),
+        "inside_8_2"=>array(
+            "link_14"=>"ref14.php",
+            "name_14"=>"Элемент 14"
+        ),
+        "inside_8_3"=>array(
+            "link_15"=>"ref15.php",
+            "name_15"=>"Элемент 15"
+        )
+    )
+);
+
+//объединяем элементы в общий массив
+$menu = array($element_1, $element_2, $element_3);
+
+// рекурсивная функция для прохода по массиву
+function level ($menu)
+{
+    foreach ($menu as $element) //проходим по первому уровню массива
+        {
+            if (is_array($element))  //если элемент массива является вложенным массивом, то вызываем функцию рекурсивно
+            {
+                echo '<ul>';
+                level($element);  //рекурсивный вызов функции
+                echo '</ul>';
+            }   elseif(preg_match("/\b.*\.php\b/", $element)) {  //если элемент не явлется массивом, то проверяем условие седержит ли значение элемента '*.php'
+                    echo '<li><a href="'.$element.'">';  //открываем тег li и обворачиваем элемент-ссылку тегами для ссылки
+            }   else {
+                    echo $element.'</a></li>';  //добавляем элемент-название и закрываем тег списка li
+            }
+        }
+}
+level($menu);  //вызываем функцию для нашего массива
