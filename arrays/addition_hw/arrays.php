d06dd2fa92df876ea621783d862de6b528f254ac
Anton <33983243+saddevil@users.noreply.github.com>
2017-12-25 23:09:17 +0300

    Update arrays.php

--- arrays/addition_hw/arrays.php
+++ arrays/addition_hw/arrays.php
@@ -1,63 +1,30 @@
-<?php
-
-$element_1 = array(
-    "link_1"=>"ref1.php",
-    "name_1"=>"Элемент 1",
-    "inside_1_1"=>array(
-        "link_4"=>"ref4.php",
-        "name_4"=>"Элемент 4",
-        "inside_4_1"=>array(
-            "link_9"=>"ref9.php",
-            "name_9"=>"Элемент 9"
-        ),
-        "inside_4_2"=>array(
-            "link_10"=>"ref10.php",
-            "name_10"=>"Элемент 10"
-        )
-    ),
-    "inside_1_2"=>array(
-        "link_5"=>"ref5.php",
-        "name_5"=>"Элемент 5"
-    )
-);
-$element_2 = array(
-    "link_2"=>"ref2.php",
-    "name_2"=>"Элемент 2",
-    "inside_2_1"=>array(
-        "link_6"=>"ref6.php",
-        "name_6"=>"Элемент 6"
-    ),
-    "inside_2_2"=>array(
-        "link_7"=>"ref7.php",
-        "name_7"=>"Элемент 7",
-        "inside_7_1"=>array(
-            "link_11"=>"ref11.php",
-            "name_11"=>"Элемент 11"
-        ),
-        "inside_7_2"=>array(
-            "link_12"=>"ref12.php",
-            "name_12"=>"Элемент 12"
-        )
-    )
-);
-$element_3 = array(
-    "link_3"=>"ref3.php",
-    "name_3"=>"Элемент 3",
-    "inside_3_1"=>array(
-        "link_8"=>"ref8.php",
-        "name_8"=>"Элемент 8",
-        "inside_8_1"=>array(
-            "link_13"=>"ref13.php",
-            "name_13"=>"Элемент 13"
-        ),
-        "inside_8_2"=>array(
-            "link_14"=>"ref14.php",
-            "name_14"=>"Элемент 14"
-        ),
-        "inside_8_3"=>array(
-            "link_15"=>"ref15.php",
-            "name_15"=>"Элемент 15"
-        )
-    )
-);
-$menu = array($element_1, $element_2, $element_3);
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Дополнительные задания</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-10">
+            <h1>Массивы</h1>
+        <h4>Задание №1</h4>
+        <?php include 'arrays_1.php';?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-10">
+        <h4>Задание №2</h4>
+        <?php include 'arrays_2.php';?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-10">
+            <h1>Строки</h1>
+            <?php include 'lines.php';?>
+        </div>
+    </div>
+</div>
+</body>
+</html>
